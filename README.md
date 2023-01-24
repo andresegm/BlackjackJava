662eefda9f802a0ec618b161850807bfe216ea80
Andres Godoy <andresegm@gmail.com>
2023-01-24 15:09:29 -0700

    Updated README instructions. Added comments to code

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # BlackjackJava
 Java game of Blackjack
 
-This is a simple game of Blackjack.
-Cards are 1-10 (inclusive).
-To start the game, type 'java main.java' in the console.
-When the game starts, the player and the dealer will be each dealt 2 cards. The dealer's second card is hidden.
-The player must choose to either "hit" or "stay". Each time the player chooses "hit" they are dealt a new card, and asked again to "hit" or "stay".
+Run the program.
 
-The objective of the game is to have the higher score while not exceeding 21. If both the player and the dealer exceed 21, the one with the lower score wins the game.
+When prompted, enter your name.
 
-The dealer will automatically hit if their score is below 17.
+The program will then initialize two sets of cards for the player and the dealer. It will also print out the initial cards and scores for the player and the dealer.
+
+The player will be prompted to either "hit" or "stay" by entering "h" or "s" respectively.
+
+If the player chooses to "hit", the program will add a new random card to the player's hand and will print out the updated cards and score for the player. If the player's score is 21 or over 21, the game will end.
+
+If the player chooses to "stay", the program will move on to the dealer's turn. The dealer will draw new cards until the dealer's score is 17 or higher, unless the player has bust.
+
+The program will then print out the final cards and scores for both the player and the dealer.
+
+The program will then determine the winner based on the scores and print the result.
+
+The game will end.
+
+Please note that this is a simple version of the game and there are many more rules and variations in actual Blackjack game.
+
+
+
--- main.java
+++ main.java
@@ -3,25 +3,43 @@ import java.util.Scanner;
 
 public class Blackjack {
 
+    //Function that allows us to add cards to get total score
+      public static int getSum(ArrayList<Integer> cards) {
+        int sum = 0;
+        for (int card : cards) {
+            sum += card;
+        }
+        return sum;
+    }
+
+    //These two arrays represent the player's cards, and the dealer's cards, which are initially empty
     static ArrayList<Integer> playerCards = new ArrayList<Integer>();
     static ArrayList<Integer> dealerCards = new ArrayList<Integer>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Initialize player cards
+        //This code prompts the user for their name, which is used through the game
+        System.out.println("What is your name? ");
+        Scanner inputName = new Scanner(System.in);
+        String playerName = inputName.nextLine();
+  
+      
+        // Initialize player's cards, dealing two random cards between 1 and 10 to the player
         playerCards.add((int) (Math.random() * 10) + 1);
         playerCards.add((int) (Math.random() * 10) + 1);
 
-        // Initialize dealer cards
+        // Initialize dealer cards, dealing two random cards between 1 and 10 to the dealer
         dealerCards.add((int) (Math.random() * 10) + 1);
         dealerCards.add((int) (Math.random() * 10) + 1);
 
-        // Print initial cards
-        System.out.println("Player's cards: " + playerCards);
+        // Print initial cards. Note that the dealer's second card is not displayed 
+        System.out.println(playerName + "'s cards: " + playerCards);
+        System.out.println(playerName + "'s score: " + getSum(playerCards));
         System.out.println("Dealer's cards: " + dealerCards.get(0) + " and [hidden]");
 
-        // Player's turn
+        // Player's turn. The player is prompted to type "h" (hit) or "s" (stay)
+        // The player's turn continues until he types "s" or busts (goes over 21)
         while (true) {
             System.out.print("Would you like to hit or stay? (h/s): ");
             String input = sc.nextLine();
@@ -29,14 +47,15 @@ public class Blackjack {
             if (input.equals("h")) {
                 int newCard = (int) (Math.random() * 10) + 1;
                 playerCards.add(newCard);
-                System.out.println("Player's cards: " + playerCards);
+                System.out.println(playerName + "'s cards: " + playerCards);
+                System.out.println(playerName + "'s score: " + getSum(playerCards));
 
                 if (getSum(playerCards) == 21) {
                     break;
                 }
 
                 if (getSum(playerCards) > 21) {
-                    System.out.println("Player busts!");
+                    System.out.println(playerName + " busts!");
                     break;
                 }
             } else if (input.equals("s")) {
@@ -44,41 +63,32 @@ public class Blackjack {
             }
         }
 
-        // Dealer's turn
-        while (getSum(dealerCards) <= 17) {
+        // Dealer's turn. The dealer only draws if their score is equal to or less than 17, and the player did not bust.
+        while (getSum(dealerCards) <= 17 && getSum(playerCards) < 22) {
             int newCard = (int) (Math.random() * 10) + 1;
             dealerCards.add(newCard);
         }
 
         // Print final cards and scores
-        System.out.println("Player's cards: " + playerCards);
-        System.out.println("Player's score: " + getSum(playerCards));
+        System.out.println(playerName + "'s cards: " + playerCards);
+        System.out.println(playerName + "'s score: " + getSum(playerCards));
         System.out.println("Dealer's cards: " + dealerCards);
         System.out.println("Dealer's score: " + getSum(dealerCards));
 
-        // Determine winner
-        if (getSum(playerCards) > 21 && getSum(dealerCards) < getSum(playerCards)) {
-            System.out.println("Player loses!");
-        } else if (getSum(dealerCards) > 21 && getSum(dealerCards) > getSum(playerCards)) {
-            System.out.println("Player wins!");
+        // Determine winner by comparing the player's score and dealer's score. If the player bust, they automatically lose.
+        if (getSum(playerCards) > 21) {
+            System.out.println(playerName + " loses!");
         } else if (getSum(playerCards) > getSum(dealerCards) && getSum(playerCards) <= 21) {
-            System.out.println("Player wins!");
+            System.out.println(playerName + " wins!");
         } else if (getSum(playerCards) < getSum(dealerCards) && getSum(dealerCards) <= 21) {
-            System.out.println("Player loses!");
+            System.out.println(playerName + " loses!");
+        } else if (getSum(playerCards) < getSum(dealerCards) && getSum(dealerCards) > 21) {
+            System.out.println(playerName + " wins!");
         } else {
             System.out.println("It's a tie!");
         }
       
     }
-
-    public static int getSum(ArrayList<Integer> cards) {
-        int sum = 0;
-
-        for (int card : cards) {
-            sum += card;
-        }
-
-        return sum;
-    }
-
 }
+
+//Please note that this is a simple version of the game and there are many more rules and variations in actual Blackjack game.
